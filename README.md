be867e88317fbf8703f5d16b56a5540d42524c90
Endre <tamas.nagy.endre@gmail.com>
2019-03-31 16:04:49 +0200

    after 7-14

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Last video
-7-6
+7-14
 
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React from 'react';
+import App from './App';
 
 
 class SearchBar extends React.Component {
-    onInputChange(event){
-        console.log(event.target.value);
-    }
+    
+    state = {term: ''};
 
+    onFormSubmit = (event) => {
+        event.preventDefault(); //prevents the browser to submit the form and reload the page
+        this.props.onSubmit(this.state.term)
+    }
 
     render(){
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange}/>
+                        <input  
+                            type="text" 
+                            value={this.state.term} 
+                            onChange={(e)=>{this.setState({term: e.target.value.toUpperCase()})}}/>
+                        <label>{this.state.term}</label>
                     </div>
                 </form>
             </div>
